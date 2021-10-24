e82b38a8ea6c5354c687e8b3297bbd8db7d2987f
= <=>
2021-10-25 00:46:09 +0200

    Definicion del schema en varios ficheros

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -2,8 +2,15 @@ import { GraphQLSchema } from 'graphql';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 
 import 'graphql-import-node';
-import typeDefs from './schema.graphql';
 import resolvers from './resolvers';
+
+// ./graphql/typeDefs.js
+import path from 'path';
+import { loadFilesSync } from '@graphql-tools/load-files';
+import { mergeTypeDefs } from '@graphql-tools/merge';
+
+const typesArray = loadFilesSync(path.join(__dirname, './graphql'), { extensions: ['graphql'] });
+const typeDefs = mergeTypeDefs(typesArray);
 
 // Construir schema ejecutable
 const schema: GraphQLSchema = makeExecutableSchema({
